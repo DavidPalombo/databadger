f04144f72a51e7493f21e7feb768844fdd58b0b1
Dave Palombo <david.palombo5@gmail.com>
2026-06-08 23:01:37 -0400

    added basic scraper test

--- tests/test_simple_scraper.py
+++ tests/test_simple_scraper.py
@@ -1,2 +1,10 @@
-def test_placeholder():
-    pass
+from src.scraper.simple_scraper import SimpleScraper
+
+def test_scrape_returns_dict():
+    scraper = SimpleScraper()
+
+    data = scraper.scrape("https://quotes.toscrape.com")
+
+    assert isinstance(data, dict)
+    assert "links" in data
+    assert "forms" in data
